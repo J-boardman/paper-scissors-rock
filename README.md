f2e16f3861980056ede95c50256b18784e56d3d0
J-boardman <jaydeboardman@outlook.com>
2021-11-12 17:13:56 +1000

    .

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
-Create function called computerPlay that will randomly return 'Rock' 'Paper' or 'Scissors'
+Sample algorithm:
 
-Write a function that plays a single round of PSR. The function should take two parameters - playerSelection & computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock".
+1. Ask computer to return a value of rock, paper or scissors, chosen at random.
 
-Make your functions playerSelection parameter case-insensitive.
+2. Ask for input from user. User can choose from rock, paper or scissors. Choice should be case insensitive.
 
-IMPORTANT NOTE: you want to return the results of this function, not console.log() them.
+3. Play a round of paper, scissors, rock, using the user and computer inputs to determine the result of the round. Return result.
 
-Write a new function called game(). Use the previous function inside this one to play a 5 round game tha tkeeps score and reports a winner or loser at the end.
+4. Since *loop* hasnt been taught yet, call the round function 5 times.
 
-You have not officially learned how to "loop" over code yet... Just call playRound function 5 times in a row.
-
-At this point you should be using console.log() to display the results of each round and the winner at the end.
-
-Use prompt to get input from the user:
-https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
-
-Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
-
-Feel free to create more "helper" functions if you think it would be more useful.
+5. Determine the winner based on 5 rounds of the game.
--- script.js
+++ script.js
@@ -1,49 +1,66 @@
-//Create function named computerPlay that *randomly* returns rock paper or scissors.
-
+//Ask computer to return a value of rock, paper, or scissors, chosen at random.
+playerSelection = "";
+computerSelection = "";
 function computerPlay(){
     const choices = ["Rock","Paper","Scissors"];
-    let computerChoice = choices[Math.floor(Math.random()*choices.length)]
+    let randomChoice = choices[Math.floor(Math.random()*choices.length)]
 
-    return computerChoice
+    console.log("Computer" + " = " + randomChoice);
+    return computerSelection = randomChoice 
 }
-//Had to look up random component.
-
-//function to get a choice from a player. Originally part of round function, but that function was getting too messy for my liking.
-
+//Ask for input from user. User should type either rock, paper or scissors, with case insensitivity
 function playerPlay(){
     choice = prompt("Pick paper, scissors, or rock!"); 
     choice = choice.toLowerCase();
     choice = choice.charAt(0).toUpperCase() + choice.slice(1);
     
     if(choice=="Paper" || choice=="Scissors" || choice=="Rock"){
-        return choice
-    } else alert ("That wasn't an option... Please reload and try again...");
+        console.log("You" + " = " + choice);
+        return playerSelection = choice
+    } else alert("That wasn't an option... You lose this round...");
 }
+//Play a round of paper,scissors,rock using the computer and user inputs as parameters. Return the result of the round
 
-//Initially these were function variables, probably makes my life easier if they're on a global scope, right???
-playerSelection = playerPlay();
-computerSelection = computerPlay();
-
-//Create function that plays a single round of PSR. Two parameters: playerSelection & computerSelection which then return a string that declares the winner. Make playerSelection case-insensitive!
-
+wins = 0;
+loses = 0;
 
 function round(){
-    
-    console.log("You = " + playerSelection)
-    console.log("Computer = " + computerSelection);
-
-//Initial drafts for this code can be found in whack.md, I'm not really sure why I thought an array was necessary...
+    playerPlay();
+    computerPlay();
     if(playerSelection=="Paper" && computerSelection=="Rock" || playerSelection=="Scissors" && computerSelection=="Paper" || playerSelection=="Rock" && computerSelection=="Scissors"){
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection + '!'
-    } else if (playerSelection==computerSelection){
-        return 'Draw! You both chose ' + playerSelection +'!'
-    } else return 'You lose! ' + computerSelection + ' beats ' + playerSelection +'!'
+        console.log ( 'You win! ' + playerSelection + ' beats ' + computerSelection + '!');
+        console.log("------------------------------")
+        return wins+=1;
+    } else if (playerSelection===computerSelection){
+        console.log( 'Draw! You both chose ' + playerSelection +'!');
+        console.log("------------------------------")
+        return wins+=0;
+    } else console.log( 'You lose! ' + computerSelection + ' beats ' + playerSelection +'!');
+        console.log("------------------------------")
+        return loses+=1;
 
-    
 }
-console.log(round())
-computerPlay();
-let newChoice = computerPlay();
-console.log(newChoice);
 
-//Write a new function called game(). Use the previous function inside this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+round(); 
+round();
+round();
+round();
+round();
+
+console.log("You = " + wins + "  ||  " + "Computer = " + loses);
+console.log("------------------------------")
+
+//End the game after 5 rounds, declare a winner.
+
+function game(){
+    if (wins>loses){
+        console.log("Wow! Over 5 rounds you came out on top with " + wins + " rounds won! Congrats!");
+        return "Winner!";
+    } else if (wins<loses){
+        console.log("Oof! Over 5 rounds you came up short... The computer beat you with " + loses + " rounds won...")
+        return "Loser!";
+    } else
+        console.log("Wow! A tie!");
+        return "Draw!"
+}
+game();
